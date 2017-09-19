b86f7deab5e107a19abf41fdda64a378a27a4bd6
Alan Kemsley <alanjkemsley@gmail.com>
2017-09-19 19:58:00 -0400

    Add initial files and basic front-end

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  
+  //Floating action button
+  $("#reservation-button").on("mouseover", function() {
+    $("#reservation-button").children("a").removeClass("pulse");
+  });
+
+  //Trigger modal
+  $(".modal").modal();
+  
+});
+  
